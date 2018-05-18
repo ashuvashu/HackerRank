bc8f8884cc95a5d1d5c3bdf31d915a45a236f4d1
ashuvashu <ashuvashu@gmail.com>
2018-05-18 01:45:35 -0700

    v1.0 Final

--- mean_mode_median.py
+++ mean_mode_median.py
@@ -1,22 +1,34 @@
-import math
-asize = int(input())
+'''
+This file is for the problem at link
+https://www.hackerrank.com/challenges/s10-basic-statistics/problem
+'''
+import statistics as stats
+from collections import Counter
+
+# Next line to take the first input
+count = input()
+
+# Next line is to take the input of the list of values
 alist = sorted([int(x) for x in input().split()])
-mean = sum(alist)/asize
-median = (sorted(alist)[math.floor((len(alist)-1)/2)]+sorted(alist)[math.floor((len(alist)-1)/2)])/2
-amap = {}
-for i in alist:
-    if i in amap:
-        amap[i] = amap[i]+1
-    else:
-        amap[i] = 1
-a = 0
-mode = 0
-for i in amap:
-    if amap[i] > a:
-        a = amap[i]
-        mode = i
-    else:
-        pass
+adict = dict(Counter(alist))
+
+# Calculating the mean and median
+mean = stats.mean(alist)
+median = stats.median(alist)
+
+mm = 0
+mn = max(alist)
+
+# Trying to check if mode has more than 1 unique values
+try:
+    mode = stats.mode(alist)
+except:
+    for j in alist:
+        if adict[j] > mm:
+            mm = adict[j]
+            mode = j
+
+
 print(mean)
-print(mean)
+print(median)
 print(mode)
