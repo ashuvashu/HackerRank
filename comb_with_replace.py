fccbe01e9b985172c2c25df2ce568f5ba74800c3
ashuvashu <ashuvashu@gmail.com>
2018-05-20 01:08:02 -0700

    Final V1.0

--- comb_with_replace.py
+++ comb_with_replace.py
@@ -1,4 +1,4 @@
 from itertools import combinations_with_replacement
 j = input().split()
-for i in sorted(list(combinations_with_replacement(j[0], int(j[1])))):
+for i in list(combinations_with_replacement(sorted(j[0]), int(j[1]))):
     print(''.join(i))
