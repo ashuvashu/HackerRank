e43521d847d09d66711be0cbb24c0d48d465aeff
ashuvashu <ashuvashu@gmail.com>
2018-05-22 23:58:28 -0700

    Draft V0.01a

--- Interquartile_range.py
+++ Interquartile_range.py
@@ -1,2 +1,10 @@
 import statistics as stats
 import math
+import numpy as np
+n = int(input())
+a = [int(x) for x in input().split()]
+b = [int(x) for x in input().split()]
+f = []
+for i in range(0,len(a)):
+    # f = f + np.repeat(a[i], b[i])
+    print(np.repeat(a[i], b[i]))
