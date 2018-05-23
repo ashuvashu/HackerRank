49fdfea95b5f557160f8e6d998fe569964521357
ashuvashu <ashuvashu@gmail.com>
2018-05-23 00:25:23 -0700

    Final V0.01

--- Interquartile_range.py
+++ Interquartile_range.py
@@ -1,13 +1,16 @@
 import statistics as stats
 import math
-import numpy as np
+# import numpy as np
 n = int(input())
 a = [int(x) for x in input().split()]
 b = [int(x) for x in input().split()]
 f = []
 for i in range(0,len(a)):
-    f = f + np.repeat(a[i], b[i]).tolist()
+    for j in range(0,b[i]):
+        # f = f + np.repeat(a[i], b[i]).tolist()
+        f.append(a[i])
     # print(f)
 g = sorted(f)
 # print(g)
-print(stats.median(g[math.ceil(len(g)/2):]) - stats.median(g[0:math.ceil(len(g)/2)]))
+p = stats.median(g[math.ceil(len(g)/2):]) - stats.median(g[0:math.ceil(len(g)/2)])
+print(round(float(p),1))
