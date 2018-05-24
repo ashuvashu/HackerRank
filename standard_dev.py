0ee2850611bb1f876b6e6e0af13ad563b3cd6cfa
ashuvashu <ashuvashu@gmail.com>
2018-05-23 23:56:09 -0700

    Final v1.0

---
+++ standard_dev.py
@@ -0,0 +1,18 @@
+import math
+c = int(input())
+x = [int(x) for x in input().split()]
+
+def stdev(x):
+    lsum = 0
+    sqsum = 0
+    for i in x:
+        lsum += i
+    mean = lsum/len(x)
+
+    for i in x:
+        sqsum += math.pow((i-mean),2)
+
+    sqmean = math.pow(sqsum/len(x),0.5)
+    return sqmean
+
+print(stdev(x))
